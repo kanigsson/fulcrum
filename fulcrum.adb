5851c9c7fa36a504911d8c15ee889ff0a218c07a
Johannes Kanig <kanig@adacore.com>
2018-04-28 17:09:00 +0900

    completed proof

--- fulcrum.adb
+++ fulcrum.adb
@@ -1,49 +1,89 @@
 package body Fulcrum with SPARK_Mode is
 
-   function Sum_Acc (S : Seq) return Seq is
-      Result : Seq (S'Range) := (others => 0);
+   function Sum_Acc (S : Seq) return Sum_Type is
+      Result : Sum_Type (S'Range) := (others => 0);
    begin
       Result (S'First) := S (S'First);
-      if S:'Length = 1 then
+      if S'Length = 1 then
          return Result;
       end if;
       for Index in S'First + 1 .. S'Last loop
-         Result (Index) := Result (Index - 1) + S (Index);
          pragma Loop_Invariant
            (Result (S'First) = S (S'First) and then
-                (for all I in S'First + 1 .. Index =>
+                (for all I in S'First + 1 .. Index - 1 =>
                      Result (I) = Result (I - 1) + S (I)));
+         pragma Loop_Invariant (
+            (for all I in S'First .. Index - 1 =>
+               abs (Result (I)) <= I * Int'Last));
+         Result (Index) := Result (Index - 1) + S (Index);
       end loop;
       return Result;
    end Sum_Acc;
 
-   function Sum_Acc_Rev (S : Seq) return Seq is
-      Result : Seq (S'Range) := (others => 0);
-      Acc : Integer := 0;
+   function Sum_Acc_Rev (S : Seq) return Sum_Type is
+      Result : Sum_Type (S'Range) := (others => 0);
    begin
-      for Index in reverse S'Range loop
-         Result (Index) := Acc + S (Index);
-         Acc := Result (Index);
+      Result (S'Last) := 0;
+      if S'Length = 1 then
+         return Result;
+      end if;
+      for Index in reverse S'First .. S'Last - 1 loop
+         pragma Loop_Invariant
+           (Result (S'Last) = 0 and then
+                (for all I in Index + 1 .. S'Last - 1 =>
+                     Result (I) = Result (I + 1) + S (I + 1)));
+         pragma Loop_Invariant (
+            (for all I in Index + 1 .. S'Last =>
+               abs (Result (I)) <= (S'Last - I + 1) * Int'Last));
+         Result (Index) := Result (Index + 1) + S (Index + 1);
       end loop;
       return Result;
    end Sum_Acc_Rev;
 
-   function Find_Fulcrum (S : Seq) return Integer is
-      Left_Sums : Seq := Sum_Acc (S);
-      Right_Sums : Seq := Sum_Acc_Rev (S);
-      Max : Integer := Left_Sums (S'First) - Right_Sums (S'First);
-      Index : Integer := S'First;
+   function Sum (S : Seq) return Integer
+      with Pre => S'Length > 0,
+      Post =>
+        Sum'Result = Sum_Acc_Rev (S) (S'First);
+
+   function Sum (S : Seq) return Integer is
+      Result : Integer := 0;
    begin
-      for I in S'First + 1 .. S'Last loop
-         if Left_Sums (I) - Right_Sums (I) > Max then
-            Max := Left_Sums (I) - Right_Sums (I);
-            Index := I;
-         end if;
-         pragma Loop_Invariant
-           ((for all K in S'First .. I =>
-                Sum_Acc (S) (K) - Sum_Acc_Rev (S) (K) <=
-                Sum_Acc (S) (Index) - Sum_Acc_Rev (S) (Index)));
+      for Index in reverse S'First + 1 .. S'Last loop
+         pragma Loop_Invariant (Result = Sum_Acc_Rev (S) (Index));
+         Result := Result + S (Index);
       end loop;
-      return Index;
-   end Find_Fulcrum;
+      return Result;
+   end Sum;
+
+  function Find_Fulcrum (S : Seq) return Nat is
+     Index : Nat := S'First;
+     Left_Sum : Integer := S (S'First);
+     Right_Sum : Integer := Sum (S);
+     Max : Integer := Left_Sum - Right_Sum;
+  begin
+     for I in S'First + 1 .. S'Last loop
+        pragma Loop_Invariant
+          (Left_Sum = Sum_Acc (S) (I - 1) and then
+           Index in S'Range and then
+           Right_Sum = Sum_Acc_Rev (S) (I - 1) and then
+           Max = Sum_Acc (S) (Index) - Sum_Acc_Rev (S) (Index) and then
+          (for all K in S'First .. I - 1 =>
+               Sum_Acc (S) (K) - Sum_Acc_Rev (S) (K) <=
+               Sum_Acc (S) (Index) - Sum_Acc_Rev (S) (Index)));
+        Left_Sum := Left_Sum + S (I);
+        Right_Sum := Right_Sum - S (I);
+        pragma Assert (Left_Sum = Sum_Acc (S) (I));
+        pragma Assert (Right_Sum = Sum_Acc_Rev (S) (I));
+        if Left_Sum - Right_Sum > Max then
+           Max := Left_Sum - Right_Sum;
+           Index := I;
+        end if;
+        pragma Assert (
+          (for all K in S'First .. I =>
+               Sum_Acc (S) (K) - Sum_Acc_Rev (S) (K) <=
+               Sum_Acc (S) (Index) - Sum_Acc_Rev (S) (Index)));
+     end loop;
+     return Index;
+  end Find_Fulcrum;
+
 end Fulcrum;
--- fulcrum.ads
+++ fulcrum.ads
@@ -1,30 +1,41 @@
 package Fulcrum with SPARK_Mode is
 
-   type Seq is array (Natural range <>) of Integer;
+   subtype Int is Integer range -1000 .. 1000;
+   subtype Nat is Integer range 1 .. 1000;
 
-   function Sum_Acc (S : Seq) return Seq
-     with Pre => S'Length > 0,
+   type Seq is array (Nat range <>) of Int;
+   type Sum_Type is array (Nat range <>) of Integer;
+
+   function Sum_Acc (S : Seq) return Sum_Type
+     with Ghost,
+     Pre => S'Length > 0,
      Post =>
      (Sum_Acc'Result'Length = S'Length and then
       Sum_Acc'Result'First = S'First and then
+      (for all I in S'First .. S'Last =>
+            abs (Sum_Acc'Result (I)) <= I * Int'Last) and then
       Sum_Acc'Result (S'First) = S (S'First) and then
       (for all I in S'First + 1 .. S'Last =>
             Sum_Acc'Result (I) = Sum_Acc'Result (I - 1) + S (I)));
 
-   function Sum_Acc_Rev (S : Seq) return Seq
-     with Pre => S'Length > 0,
+   function Sum_Acc_Rev (S : Seq) return Sum_Type
+     with Ghost,
+     Pre => S'Length > 0,
      Post =>
-       (Sum_Acc_Rev'Result'Length = S'Length and then
+     (Sum_Acc_Rev'Result'Length = S'Length and then
       Sum_Acc_Rev'Result'First = S'First and then
+      (for all I in S'First .. S'Last =>
+         abs (Sum_Acc_Rev'Result (I)) <= (S'Last - I + 1) * Int'Last) and then
       Sum_Acc_Rev'Result (S'Last) = 0 and then
       (for all I in S'First .. S'Last - 1 =>
-            Sum_Acc_Rev'Result (I) = Sum_Acc_Rev'Result (I + 1) + S (I)));
+            Sum_Acc_Rev'Result (I) = Sum_Acc_Rev'Result (I + 1) + S (I + 1)));
 
-   function Find_Fulcrum (S : Seq) return Integer
-   with Pre => S'Length > 0,
-        Post =>
-        ((for all I in S'Range =>
-                Sum_Acc (S) (I) - Sum_Acc_Rev (S) (I) <=
-                Sum_Acc (S) (Find_Fulcrum'Result) - Sum_Acc_Rev (S) (Find_Fulcrum'Result)));
+  function Find_Fulcrum (S : Seq) return Nat
+  with Pre => S'Length > 0,
+       Post =>
+       (Find_Fulcrum'Result in S'Range and then
+          (for all I in S'Range =>
+               Sum_Acc (S) (I) - Sum_Acc_Rev (S) (I) <=
+               Sum_Acc (S) (Find_Fulcrum'Result) - Sum_Acc_Rev (S) (Find_Fulcrum'Result)));
 
 end Fulcrum;
